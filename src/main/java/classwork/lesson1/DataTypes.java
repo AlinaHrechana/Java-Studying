fcbe25e8409e64e104b7db0059d34c12794655e1
AlinaHrechana <avoalloe10@gmail.com>
2026-08-08 18:16:33 +0300

    done homework

---
+++ src/main/java/classwork/lesson1/DataTypes.java
@@ -0,0 +1,35 @@
+package classwork.lesson1;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        byte byteVariable = 5;
+        System.out.println("byteVariable: " + byteVariable);
+
+        short shortVariable = 32_000;
+        System.out.println("shortVariable: " + shortVariable);
+
+        int intVariable = 2_000_000_000;
+        System.out.println("intVariable: " + intVariable);
+
+        long longVariable = 2_000_000_000_000L;
+        System.out.println("longVariable: " + longVariable);
+
+        float floatVariable = 34.5676F;
+        System.out.println("floatVariable: " + floatVariable);
+
+        double doubleVariable = 34.71565127;
+        System.out.println("doubleVariable: " + doubleVariable);
+
+        boolean booleanVariable = true; // может быть и false
+        System.out.println("booleanVariable: " + booleanVariable);
+
+        char charVariable = 'c';
+        System.out.println("charVariable: " + charVariable);
+
+        char charNumVariable = 654;
+        System.out.println("charNumVariable: " + charNumVariable);
+    }
+}
+
+
---
+++ src/main/java/classwork/lesson1/Main.java
@@ -0,0 +1,19 @@
+package classwork.lesson1;
+
+/*
+
+Shift + F6 - переименовать
+Shift + F10 - Запустить
+psvm - public static void main
+sout - System.out.println()
+
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("new text\n"); // ручной перенос на новую строку \n
+        System.out.println("hello world"); //comment
+    }
+
+}

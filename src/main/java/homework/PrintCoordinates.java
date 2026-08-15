c30cfd49692128e50083974079fc8069066e8abb
AlinaHrechana <avoalloe10@gmail.com>
2026-08-15 18:11:46 +0300

    Added first homework

---
+++ src/main/java/homework/PrintCoordinates.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class PrintCoordinates {
+
+    public static void main(String[] args) {
+
+        float latitude = 40.4121F;
+        float longitude = 74.0240F;
+
+        System.out.println("Coordinates of the Statue of Liberty are latitude: " + latitude + " longitude: " + longitude);
+    }
+}
---
+++ src/main/java/homework/SwapVariables.java
@@ -0,0 +1,23 @@
+package homework;
+
+public class SwapVariables {
+
+    public static void main(String[] args) {
+
+        int a= 10;
+        int b= 20;
+        int c= 0;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        c=a;
+        a=b;
+        b=c;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+
+}

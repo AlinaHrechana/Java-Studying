26ec132687d7854c6466aa1b8ae9d758a5a5f786
AlinaHrechana <avoalloe10@gmail.com>
2026-09-04 21:48:21 +0300

    did homework

--- src/main/java/homework/homework4/Person.java
+++ src/main/java/homework/homework4/Person.java
@@ -4,22 +4,22 @@ public class Person {
 
     public static void main(String[] args) {
 
-        personInfo("Will ", "Smith ", "New York ", "2936729462846");
+        System.out.println(personInfo("Will ", "Smith ", "New York ", "2936729462846"));
 
-        personInfo("Jackie ", "Chan ", "Shanghai ", "12312412412");
+        System.out.println(personInfo("Jackie ", "Chan ", "Shanghai ", "12312412412"));
 
-        personInfo("Sherlock ", "Holmes ", "London ", "37742123513");
+        System.out.println(personInfo("Sherlock ", "Holmes ", "London ", "37742123513"));
 
-        personInfo("Lionel ", "Messi ", "Miami ", "13035556123 ", 33125);
+        System.out.println(personInfo("Lionel ", "Messi ", "Miami ", "13035556123 ", 33125));
     }
 
-    public static void personInfo(String name, String surname, String city, String phoneNumber) {
+    public static String personInfo(String name, String surname, String city, String phoneNumber) {
 
-        System.out.println("Позвонить гражданину " + name + surname + "из города " + city + "можно по номеру " + phoneNumber);
+       return "Позвонить гражданину " + name + surname + "из города " + city + "можно по номеру " + phoneNumber;
     }
 
-    public static void personInfo(String name, String surname, String city, String phoneNumber, int postalCode){
+    public static String personInfo(String name, String surname, String city, String phoneNumber, int postalCode){
 
-        System.out.println("Позвонить гражданину " + name + surname + "из города " + city + "можно по номеру " + phoneNumber + "почтовый код " + postalCode);
+        return "Позвонить гражданину " + name + surname + "из города " + city + "можно по номеру " + phoneNumber + "почтовый код " + postalCode;
     }
 }

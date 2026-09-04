d76c83d51329c400ab30f7a5a6a32154e4615a57
AlinaHrechana <avoalloe10@gmail.com>
2026-09-04 19:51:50 +0300

    did homework

--- src/main/java/homework/homework4/Person.java
+++ src/main/java/homework/homework4/Person.java
@@ -9,10 +9,17 @@ public class Person {
         personInfo("Jackie ", "Chan ", "Shanghai ", "12312412412");
 
         personInfo("Sherlock ", "Holmes ", "London ", "37742123513");
+
+        personInfo("Lionel ", "Messi ", "Miami ", "13035556123 ", 33125);
     }
 
-    public static void personInfo( String name, String surname, String city, String phoneNumber){
+    public static void personInfo(String name, String surname, String city, String phoneNumber) {
 
-        System.out.println("Позвонить гражданину " +  name + surname + "из города " + city + "можно по номеру " + phoneNumber);
+        System.out.println("Позвонить гражданину " + name + surname + "из города " + city + "можно по номеру " + phoneNumber);
+    }
+
+    public static void personInfo(String name, String surname, String city, String phoneNumber, int postalCode){
+
+        System.out.println("Позвонить гражданину " + name + surname + "из города " + city + "можно по номеру " + phoneNumber + "почтовый код " + postalCode);
     }
 }
